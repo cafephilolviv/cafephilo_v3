061fd2776617b89846b3deaf7ed0de56c3fb5730
Volodymyr Yevtushenko <v.yev@tuta.io>
2019-10-09 21:42:10 +0300

    Add deps and rubocop

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+Dotenv::Railtie.load
 module CafephiloV3
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
