9990552de2e013d8a44ed59f5df9ad29d47aa073
ihortok <itokaryk@gmail.com>
2019-11-14 22:36:44 +0200

    about view & controller

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class AboutController < ApplicationController
-  def index; end
+  def index
+    members = Member.all
+    facts = Fact.all
+    render :index, locals: { members: members, facts: facts }
+  end
 end
