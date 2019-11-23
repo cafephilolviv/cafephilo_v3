c40e3d550512c2a2e7a5ddf47ce710e1e77584ba
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2019-11-23 16:07:23 +0200

    Add master key task

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,3 +6,17 @@ set :repo_url, "git@gitlab.com:cafephilolviv/cafephilo_v3.git"
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
 # set :linked_files, %w{config/master.key}
+
+append :linked_files, "config/master.key"
+
+namespace :deploy do
+  namespace :check do
+    before :linked_files, :set_master_key do
+      on roles(:app), in: :sequence, wait: 10 do
+        unless test("[ -f #{shared_path}/config/master.key ]")
+          upload! 'config/master.key', "#{shared_path}/config/master.key"
+        end
+      end
+    end
+  end
+end
