7c957e41691782269a1aa5e8eb46822bdeac21be
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2019-11-25 00:37:03 +0200

    Add envs for prod db

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@ server '159.69.209.127',
        roles: %w[app db web]
 
 set :project_name, 'cafephilo_v3_staging'
-set :branch, 'develop'
+set :branch, 'master'
 set :deploy_to, '/home/cafephilo_deploy/site/'
 set :normalize_asset_timestamps,
     %w[public/images public/javascripts public/stylesheets]
