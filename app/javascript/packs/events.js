d9020f1a7dbd8540a4b5c754834ee22de5260c52
ihortok <itokaryk@gmail.com>
2020-11-25 21:28:40 +0200

    desc text height checker

--- app/javascript/packs/events.js
+++ app/javascript/packs/events.js
@@ -1,13 +1,22 @@
 $(document).on('turbolinks:load', function () {
+    $.each($('.event-desc'), function() {
+        if($(this).outerHeight() > $(this).children('.event-desc-inner').outerHeight()) {
+            $(this).removeClass('with-height');
+            $(`.event-links[data-card-id=${$(this).attr('data-card-id')}]`).toggleClass('justify-content-between justify-content-end')
+                                                                           .children('.read-more')
+                                                                           .addClass('d-none');
+        }
+    })
+
     $('.read-more').on('click', function () {
-        let desc = $(this).parents('.event-row__info').children('.event-row__desc'),
-            text_height = desc.children('.event-row__desc-inner').outerHeight();
-        $(this).toggleClass('active')
+        let desc = $(`.event-desc[data-card-id=${$(this).parent().attr('data-card-id')}]`),
+            text_height = desc.children('.event-desc-inner').outerHeight();
+        $(this).toggleClass('active');
 
         if ($(this).hasClass('active')) {
-            desc.css('height', text_height)
+            desc.css('height', text_height);
         } else {
-            desc.removeAttr('style')
+            desc.removeAttr('style');
         }
     })
 });
