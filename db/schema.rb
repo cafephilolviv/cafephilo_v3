b2f310756e1a6558a01e153d1de275150777d13e
Volodymyr Yevtushenko <voloyev@pm.me>
2021-01-20 13:14:42 +0200

    Update rails 6.1 configs (#23)
    * Fix deploy and rails admin for rails 6.1
    
    * Update rails6.1 configs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_23_124039) do
+ActiveRecord::Schema.define(version: 2021_01_20_110125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,14 @@ ActiveRecord::Schema.define(version: 2020_03_23_124039) do
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
     t.datetime "created_at", null: false
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "email_subscriptions", force: :cascade do |t|
@@ -110,4 +117,5 @@ ActiveRecord::Schema.define(version: 2020_03_23_124039) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
