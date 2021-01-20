33b7956dcc9933c6eccf2ccea8eaff421b1f14d7
Volodymyr Yevtushenko <voloyev@pm.me>
2021-01-20 13:10:13 +0200

    Update rails6.1 configs

--- config/application.rb
+++ config/application.rb
@@ -1,19 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,23 +8,15 @@ Bundler.require(*Rails.groups)
 
 module CafephiloV3
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    config.active_job.queue_adapter = :sidekiq
-    config.generators.system_tests = nil
-
-    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-    config.i18n.available_locales = %i[en uk]
-    config.to_prepare do
-      # Configure single controller layout
-      Devise::SessionsController.layout 'admin'
-      Devise::PasswordsController.layout 'admin'
-    end
-
-    # config.i18n.enforce_available_locales = false
-    Raven.configure do |config|
-      config.dsn = ENV['SENTRY_DSN']
-      config.environments = %w[staging production]
-    end
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
