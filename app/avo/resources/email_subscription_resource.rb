a501c240fddf2be10e3ebed4c105de359f0289c0
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2023-03-19 23:20:43 +0200

    Add email subscription dashboard

---
+++ app/avo/resources/email_subscription_resource.rb
@@ -0,0 +1,11 @@
+class EmailSubscriptionResource < Avo::BaseResource
+  self.title = :uuid
+  self.includes = []
+  # self.search_query = -> do
+  #   scope.ransack(id_eq: params[:q], m: "or").result(distinct: false)
+  # end
+
+  field :uuid, as: :text
+  field :email, as: :text
+  # add fields here
+end
